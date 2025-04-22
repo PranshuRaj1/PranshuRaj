4f17c3b9409435e4c7001ca64b8fa5ab815486b2
Pranshu Raj <rajpranshu2222@gmail.com>
2025-04-22 19:55:10 +0530

    Initial commit

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["ibb.co"]
-  }
-};
-
-export default nextConfig;
--- src/data/index.ts
+++
@@ -1,5 +0,0 @@
-import navItems from "@/data/nav-items";
-import projects from "@/data/projects";
-import experiences from "@/data/experiences";
-
-export { navItems, projects, experiences };
